bb4d14d7cf96037c2844ff929a09fc2600ee91b9
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-12-12 13:02:13 +0300

    First commit

---
+++ src/main/java/directory/Employee.java
@@ -0,0 +1,14 @@
+package directory;
+
+public class Emploloyee {
+    int servicenumber;
+    int experience;
+    String phonenumber;
+    String name;
+    Emploloyee(int servicenumber, String name, String phonenumber, int experience){
+        this.experience=experience;
+        this.name=name;
+        this.phonenumber=phonenumber;
+        this.servicenumber=servicenumber;
+    }
+}
